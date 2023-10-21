87a8fe3ce1b34cd98a1f6a8f33d698647f5606a5
Krishna R Acharya <krishna.acharya@radisys.com>
2023-10-21 20:52:50 +0530

    feat: Added support to use cors in the backend api

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,19 @@
 import express from "express";
 import postsRouter from "./routes/posts.js";
 import connectDB from "./config/db.js";
-
+import cors from "cors";
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 // Connect to database
 connectDB();
 
 // API route
-app.use("/posts", postsRouter);
+app.use("/api/posts", postsRouter);
 
 app.listen(port, () => {
 	console.log(`Server is running on port ${port}`);
