5e0c8defd1f28b6dc2a3a1207256aa2d723bf2dc
Krishna R Acharya <krishna.acharya@radisys.com>
2023-10-22 14:39:00 +0530

    feat: Enhance details blog page

--- frontend/src/pages/details-page.tsx
+++ frontend/src/pages/details-page.tsx
@@ -1,16 +1,12 @@
-import { useLocation } from "react-router-dom";
-interface Post {
-	_id: string;
-	authorName: string;
-	title: string;
-	imageLink: string;
-	timeOfPost: string;
-	description: string;
-	categories: string[];
-}
+import { useLocation, useNavigate } from "react-router-dom";
+import formatPostTime from "../utils/format-post-time";
+import Post from "../types/post-type";
+import { FiArrowLeft } from "react-icons/fi"; // Assuming you have an arrow icon from a library
+
 const DetailsPage = () => {
 	const { state } = useLocation();
 	const post: Post = state?.post;
+	const navigate = useNavigate();
 	return (
 		<div className="font-[Poppins]">
 			<div className="relative">
@@ -19,29 +15,35 @@ const DetailsPage = () => {
 					alt={post.title}
 					className="h-[460px] object-cover w-full "
 				/>
+
 				<div className="absolute top-0 left-0 w-full h-full bg-black opacity-50"></div>
-				<div className="px-64 absolute bottom-16 left-4 text-white">
-					<h1 className="text-3xl font-semibold">{post.title}</h1>
-					<div className="flex space-x-2 mt-6">
+				<div className="px-64 text-white absolute top-16 left-0 cursor-pointer">
+					<FiArrowLeft
+						style={{ fontSize: "36px" }}
+						onClick={() => navigate(-1)}
+					/>
+				</div>
+				<div className="px-64 absolute bottom-16  text-white">
+					<div className="flex space-x-2 mb-6">
 						{post.categories.map((category, index) => (
 							<div
 								key={index}
-								className="bg-blue-500 text-white rounded-full px-3 py-1 text-sm"
+								className="bg-gray-500 text-white rounded-full px-3 py-1 text-sm"
 							>
 								{category}
 							</div>
 						))}
 					</div>
+					<h1 className="text-4xl font-semibold ">{post.title}</h1>
 				</div>
 			</div>
 			<div className="px-64">
 				<div className="mt-4">
-					<p className="text-gray-700">{post.description}</p>
+					<p className="text-lg text-gray-700">{post.description}</p>
 				</div>
-				<div className="mt-4 text-gray-600">
-					<p>
-						{post.authorName} • {post.timeOfPost}
-					</p>
+				<div className="mt-4 ">
+					<p className=" font-semibold text-gray-600">By {post.authorName}</p>
+					<p className="text-sm">{formatPostTime(post.timeOfPost)}</p>
 				</div>
 			</div>
 		</div>
