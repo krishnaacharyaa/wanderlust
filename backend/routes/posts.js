700824bea6284c582b8254734b400e84e564b77f
Krishna R Acharya <krishna.acharya@radisys.com>
2023-10-22 11:00:13 +0530

    feat: Added endpoints for category, featured and latest posts

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -23,6 +23,38 @@ router.get("/", async (req, res) => {
 	}
 });
 
+// Route to get featured posts
+router.get("/featured-posts", async (req, res) => {
+	try {
+		const featuredPosts = await Post.find({ isFeaturedPost: true });
+		res.json(featuredPosts);
+	} catch (err) {
+		res.status(500).json({ message: err.message });
+	}
+});
+
+// Route to get posts by category
+router.get("/category/:category", async (req, res) => {
+	const category = req.params.category;
+	try {
+		const categoryPosts = await Post.find({ categories: category });
+		res.json(categoryPosts);
+	} catch (err) {
+		res.status(500).json({ message: err.message });
+	}
+});
+
+// Route for fetching the latest posts
+router.get("/latestposts", async (req, res) => {
+	try {
+		console.log("Hitting latest posts");
+		const latestPosts = await Post.find().sort({ timeOfPost: -1 });
+		console.log(latestPosts);
+		res.json(latestPosts);
+	} catch (err) {
+		res.status(500).json({ message: err.message });
+	}
+});
 // Get a specific post by ID
 router.get("/:id", async (req, res) => {
 	try {
