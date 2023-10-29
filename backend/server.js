f87bc6541596345a5b3567a55e1eb7fadbe73b6e
Darshaan Aghicha <dsaghicha211@gmail.com>
2023-10-29 12:38:16 +0530

    Implemented centralized error handler method.

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,26 @@
-import express from "express";
-import postsRouter from "./routes/posts.js";
-import connectDB from "./config/db.js";
-import cors from "cors";
-const app = express();
-const port = process.env.PORT || 5000;
+import express from 'express'
+import postsRouter from './routes/posts.js'
+import connectDB from './config/db.js'
+import cors from 'cors'
+const app = express()
+const port = process.env.PORT || 5000
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cors())
 
 // Connect to database
-connectDB();
+connectDB()
 
 // API route
-app.use("/api/posts", postsRouter);
+app.use('/api/posts', postsRouter)
 
-app.get("/", (req, res) => {
-	res.send("Yay!! Backend of wanderlust app is now accessible ");
-});
+app.get('/', (req, res) => {
+	res.send('Yay!! Backend of wanderlust app is now accessible ')
+})
 
 app.listen(port, () => {
-	console.log(`Server is running on port ${port}`);
-});
+	console.log(`Server is running on port ${port}`)
+})
 
-export default app;
+export default app
