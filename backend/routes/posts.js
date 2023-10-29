87defd9131ecfe30c4bfc92fd18ebf90091df1ae
Darshaan Aghicha <dsaghicha211@gmail.com>
2023-10-29 12:36:48 +0530

    Refactored code to go to central error handler

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,88 +1,106 @@
-import { Router } from "express";
-const router = Router();
-import Post from "../models/post.js";
+import { Router } from 'express'
+const router = Router()
+import Post from '../models/post.js'
 
 // Create a new post
-router.post("/", async (req, res) => {
+router.post('/', async (req, res, next) => {
+	console.log('Create new post triggered')
 	try {
-		const post = new Post(req.body);
-		const savedPost = await post.save();
-		res.json(savedPost);
+		const { authorName, title, imageLink, categories, description } = req.body
+		if (!authorName || !title || !imageLink || categories.length < 1 || !description) {
+			req.clientError = true
+			throw 'Missing one or more details'
+		}
+		const post = new Post(req.body)
+		const savedPost = await post.save()
+		res.json(savedPost)
 	} catch (err) {
-		res.status(400).json({ message: err.message });
+		next(err)
 	}
-});
+})
 
 // Get all posts
-router.get("/", async (req, res) => {
+router.get('/', async (_, res, next) => {
+	console.log('Get all posts triggered')
 	try {
-		const posts = await Post.find();
-		res.json(posts);
+		const posts = await Post.find()
+		console.log(posts)
+		res.json(posts)
 	} catch (err) {
-		res.status(500).json({ message: err.message });
+		next(err)
 	}
-});
+})
 
 // Route to get featured posts
-router.get("/featured-posts", async (req, res) => {
+router.get('/featured-posts', async (req, res, next) => {
+	console.log('Get featured posts triggered')
 	try {
-		const featuredPosts = await Post.find({ isFeaturedPost: true });
-		res.json(featuredPosts);
+		const featuredPosts = await Post.find({ isFeaturedPost: true })
+		res.json(featuredPosts)
 	} catch (err) {
-		res.status(500).json({ message: err.message });
+		next(err)
 	}
-});
+})
 
 // Route to get posts by category
-router.get("/category/:category", async (req, res) => {
-	const category = req.params.category;
+router.get('/category/:category', async (req, res, next) => {
+	console.log('Get category post triggered')
+	const category = req.params.category
 	try {
-		const categoryPosts = await Post.find({ categories: category });
-		res.json(categoryPosts);
+		const categoryPosts = await Post.find({ categories: category })
+		res.json(categoryPosts)
 	} catch (err) {
-		res.status(500).json({ message: err.message });
+		req.from = 'Category'
+		next(err)
 	}
-});
+})
 
 // Route for fetching the latest posts
-router.get("/latestposts", async (req, res) => {
+router.get('/latestposts', async (_, res, next) => {
+	console.log('Get latest post triggered')
 	try {
-		const latestPosts = await Post.find().sort({ timeOfPost: -1 });
-		res.json(latestPosts);
+		const latestPosts = await Post.find().sort({ timeOfPost: -1 })
+		res.json(latestPosts)
 	} catch (err) {
-		res.status(500).json({ message: err.message });
+		next(err)
 	}
-});
+})
 // Get a specific post by ID
-router.get("/:id", async (req, res) => {
+router.get('/:id', async (req, res, next) => {
+	console.log('Get post triggered')
 	try {
-		const post = await Post.findById(req.params.id);
-		res.json(post);
+		const post = await Post.findById(req.params.id)
+		res.json(post)
 	} catch (err) {
-		res.status(404).json({ message: "Post not found" });
+		req.from = 'Post'
+		next(err)
 	}
-});
+})
 
 // Update a post by ID
-router.patch("/:id", async (req, res) => {
+router.patch('/:id', async (req, res, next) => {
+	console.log('Update post triggered')
 	try {
 		const updatedPost = await Post.findByIdAndUpdate(req.params.id, req.body, {
-			new: true,
-		});
-		res.json(updatedPost);
+			new: true
+		})
+		res.json(updatedPost)
 	} catch (err) {
-		res.status(404).json({ message: "Post not found" });
+		req.from = 'Post'
+		next(err)
 	}
-});
+})
 
 // Delete a post by ID
-router.delete("/:id", async (req, res) => {
+router.delete('/:id', async (req, res, next) => {
+	console.log('Delete post triggered')
 	try {
-		await Post.findByIdAndRemove(req.params.id);
-		res.json({ message: "Post deleted" });
+		await Post.findByIdAndRemove(req.params.id)
+		res.json({ message: 'Post deleted' })
 	} catch (err) {
-		res.status(404).json({ message: "Post not found" });
+		req.from = 'Post'
+		next(err)
 	}
-});
+})
 
-export default router;
+export default router
