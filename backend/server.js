2d6b920598ff8857dcb28fb3095b5e21b83528d0
Darshaan Aghicha <dsaghicha211@gmail.com>
2023-10-29 13:53:48 +0530

    Implementing clustering

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ import express from 'express'
 import postsRouter from './routes/posts.js'
 import connectDB from './config/db.js'
 import cors from 'cors'
+import { cpus } from 'os'
+import cluster from 'cluster'
 const app = express()
 const port = process.env.PORT || 5000
 
@@ -19,8 +21,27 @@ app.get('/', (req, res) => {
 	res.send('Yay!! Backend of wanderlust app is now accessible ')
 })
 
-app.listen(port, () => {
-	console.log(`Server is running on port ${port}`)
+const nCpus = cpus().length
+if (cluster.isPrimary) {
+	for (let index = 0; index < nCpus; index++) {
+		cluster.fork()
+	}
+} else {
+	app.listen(port, () => {
+		console.log(`Server is running on port ${port}, PID: ${process.pid}`)
+	})
+}
+
+// To respawn new worker when one gets killed
+cluster.on('exit', (worker, _, __) => {
+	console.log(`Worker ${worker.pid} has been killed`)
+	console.log('Starting new worker')
+	cluster.fork()
+})
+
+process.on('unhandledRejection', (reason, promise) => {
+	console.error('Unhandled Rejection at:', promise, 'reason:', reason)
+	process.exit(1)
 })
 
 export default app
