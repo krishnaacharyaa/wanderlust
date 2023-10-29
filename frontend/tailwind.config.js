a20fa183e4e49e84d0112f563768ee29d9d8dbdd
Sankalp <sanklpkaushik21@gmail.com>
2023-10-29 17:06:49 +0530

    01_Responsive_layout

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    container: {},
+  },
+  extend: {
+    screens: {
+      sm: "640px", // Small screens
+      lg: "1024px", // Large screens
+    },
+  },
 };
