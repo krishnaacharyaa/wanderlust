ef4ffb4cdc36fe019323e1698f18a230abec4717
hemant14050 <hemant14050@gmail.com>
2023-11-10 17:12:43 +0530

    refractor: added constants file to track valid categories

---
+++ backend/utils/constants.js
@@ -0,0 +1,13 @@
+const validCategories = [
+    'Travel',
+    'Nature',
+    'City',
+    'Adventure',
+    'Beaches',
+    'Landmarks',
+    'Mountains',
+];
+
+module.exports = {
+    validCategories,
+};
