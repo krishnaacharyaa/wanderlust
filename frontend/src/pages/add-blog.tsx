a687e59eeeb3aa4df59992f1ae5efec4f25b4e3c
Jinendra Jain <jjinendra3@gmail.com>
2023-11-10 19:24:25 +0530

    enhance-issue49 Made a modal to choose from webp images

--- frontend/src/pages/add-blog.tsx
+++ frontend/src/pages/add-blog.tsx
@@ -6,7 +6,7 @@ import 'react-toastify/dist/ReactToastify.css';
 import navigateBackBlackIcon from '../assets/svg/navigate-back-black.svg';
 import { CATEGORIES } from '../constants/categories';
 import { categoryProps } from '../utils/category-props';
-import { imageUrls } from '../constants/images';
+import ModalComponent from '../components/modal';
 type FormData = {
   title: string;
   authorName: string;
@@ -16,7 +16,7 @@ type FormData = {
   isFeaturedPost: boolean;
 };
 function AddBlog() {
-  const [selectedImage, setSelectedImage] = useState<string | null>("");
+  const [selectedImage, setSelectedImage] = useState<string>('');
 
   const handleImageSelect = (imageUrl: string) => {
     setSelectedImage(imageUrl);
@@ -51,13 +51,9 @@ function AddBlog() {
     }
   };
   const handleselector = () => {
-    var url = "";
-    if (typeof selectedImage === 'string') {
-      url = selectedImage;
-    } 
     setFormData({
       ...formData,
-      imageLink: url,
+      imageLink: selectedImage,
     });
     setmodal(false);
   };
@@ -77,7 +73,7 @@ function AddBlog() {
     }
     const imageLinkRegex = /\.(jpg|jpeg|png|webp)$/i;
     if (!imageLinkRegex.test(formData.imageLink)) {
-      toast.error('Image URL must end with .jpg, .jpeg, or .png');
+      toast.error('Image URL must end with .jpg, .jpeg, .webp or .png');
       return false;
     }
     if (formData.categories.length > 3) {
@@ -139,8 +135,8 @@ function AddBlog() {
             onChange={handleInputChange}
           />
         </div>
-        <div className="flex">
-          <div className="mb-4 flex flex-1 mr-16">
+        <div className="flex justify-between">
+          <div className="flex w-8/12">
             <input
               type="text"
               id="imgtext"
@@ -153,7 +149,7 @@ function AddBlog() {
           </div>
           <button
             type="button"
-            className="bg-black text-white items-center justify-center text-base p-2 rounded-lg hover:bg-gray-800 w-full md:w-fit flex flex-2"
+            className="bg-black text-white  rounded-lg hover:bg-gray-800 px-3"
             onClick={() => {
               setmodal(true);
             }}
@@ -211,73 +207,13 @@ function AddBlog() {
           Create Blog
         </button>
       </form>
-      {modal && ( //MODAL STARTS
-        <div
-          className="relative z-10"
-          aria-labelledby="modal-title"
-          role="dialog"
-          aria-modal="false"
-        >
-          <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
-
-          <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
-            <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-              <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
-                <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
-                  <div className="sm:flex sm:items-start">
-                    <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
-                      <h3
-                        className="text-base font-semibold leading-6 text-gray-900"
-                        id="modal-title"
-                      >
-                        Select Post Image
-                      </h3>
-                      <div className="mt-2">
-                        <div className="grid grid-cols-3 gap-4">
-                          {imageUrls.map((imageUrl) => (
-                            <div
-                              key={imageUrl}
-                              className={`cursor-pointer p-2 border ${
-                                selectedImage === imageUrl ? 'border-blue-500' : 'border-gray-300'
-                              } rounded-md`}
-                              onClick={() => handleImageSelect(imageUrl)}
-                            >
-                              <img
-                                src={imageUrl}
-                                alt={`Image ${imageUrl}`}
-                                className="w-full h-full object-cover"
-                              />
-                            </div>
-                          ))}
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
-                  <button
-                    type="button"
-                    name="imageLink"
-                    className="inline-flex w-full justify-center rounded-md bg-green-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-green-500 sm:ml-3 sm:w-auto"
-                    onClick={handleselector}
-                  >
-                    Select
-                  </button>
-                  <button
-                    type="button"
-                    className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
-                    onClick={() => {
-                      setmodal(false);
-                    }}
-                  >
-                    Cancel
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div> //MODAL ENDS
-      )}
+      <ModalComponent
+        selectedImage={selectedImage}
+        handleImageSelect={handleImageSelect}
+        handleSelector={handleselector}
+        setModal={setmodal}
+        modal={modal}
+      />
     </div>
   );
 }
