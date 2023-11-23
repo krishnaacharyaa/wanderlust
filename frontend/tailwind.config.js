e3df0f4608a57550eeaf30031edc84e377d56257
Sankalp <sanklpkaushik21@gmail.com>
2023-11-23 10:14:00 +0530

    Dark Theme

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'media',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        dark: {
+          background: 'var(--color-dark-background)',
+          background_card_textfield: 'var(--color-dark-textfield-card)',
+        },
+      },
+    },
   },
   plugins: [],
 };
