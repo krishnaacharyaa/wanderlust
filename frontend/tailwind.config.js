e9a320af717fee342fce5ece8faf68842639e6ee
Chinmay Kunkikar <chinmay.kunkikar@gmail.com>
2023-12-01 03:47:21 +0530

    feat: Add `dark` color to the theme Even if they are repetitive values, they remove the guesswork while setting the color to an element.

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors');
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'media',
@@ -6,9 +7,16 @@ export default {
     extend: {
       colors: {
         dark: {
-          DEFAULT: 'var(--color-dark-background)',
-          textfield: 'var(--color-dark-textfield-card)',
+          DEFAULT: colors.slate[900],
+          primary: colors.slate[50],
+          secondary: colors.slate[300],
+          tertiary: colors.slate[400],
+          card: colors.slate[800],
+          title: colors.slate[50],
+          description: colors.slate[300],
+          info: colors.slate[400],
         },
+        light: {},
       },
     },
   },
