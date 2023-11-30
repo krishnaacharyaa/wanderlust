583eb970ea4af568e38a79967fa5927965cd4db7
Chinmay Kunkikar <chinmay.kunkikar@gmail.com>
2023-12-01 04:07:14 +0530

    refactor: Overhaul the homepage - Embrace the slate color more. - Add some visual hierarchy to different sections of the homepage. - Change Many Title Case Text Elements to Sentence case elements. - Add a hover state to the Create post button.

--- frontend/src/components/featured-post-card.tsx
+++ frontend/src/components/featured-post-card.tsx
@@ -7,17 +7,17 @@ export default function FeaturedPostCard({ post }: { post: Post }) {
   const navigate = useNavigate();
   return (
     <div
-      className="flex h-48 cursor-pointer gap-4 rounded-lg bg-slate-50 dark:bg-dark-card"
+      className="flex h-48 cursor-pointer gap-2 rounded-lg bg-slate-50 dark:bg-dark-card"
       onClick={() => navigate('/details-page', { state: { post } })}
     >
       <div className="w-1/3">
         <img
           src={post.imageLink}
           alt={post.title}
-          className="h-full w-full rounded-lg object-cover"
+          className="h-full w-full rounded-lg object-cover shadow-lg"
         />
       </div>
-      <div className="flex h-full w-2/3 flex-col gap-2">
+      <div className="flex h-full w-2/3 flex-col gap-2 p-2">
         <div className="text-xl font-semibold dark:text-dark-title">{post.title}</div>
         <div className="flex flex-wrap gap-2">
           {post.categories.map((category, index) => (
@@ -29,7 +29,7 @@ export default function FeaturedPostCard({ post }: { post: Post }) {
         <div className="line-clamp-2 text-slate-700">
           <p className="overflow-ellipsis dark:text-dark-description">{post.description}</p>
         </div>
-        <div className="mb-1 flex flex-1 items-end text-xs text-slate-600 dark:text-dark-info">
+        <div className="flex flex-1 items-end text-xs text-slate-600 dark:text-dark-info">
           {post.authorName} • {formatPostTime(post.timeOfPost)}
         </div>
       </div>
--- frontend/src/pages/home-page.tsx
+++ frontend/src/pages/home-page.tsx
@@ -7,6 +7,7 @@ import PostCard from '@/components/post-card';
 import Post from '@/types/post-type';
 import { PostCardSkeleton } from '@/components/skeletons/post-card-skeleton';
 function HomePage() {
+  const navigate = useNavigate();
   const [posts, setPosts] = useState<Post[]>([]);
 
   useEffect(() => {
@@ -19,19 +20,19 @@ function HomePage() {
         console.error(error);
       });
   }, []);
-  const navigate = useNavigate();
+
   return (
-    <div className="w-full bg-slate-50 font-[Poppins] dark:bg-dark">
+    <div className="w-full cursor-default bg-slate-50 font-[Poppins] dark:bg-dark">
       <div
         style={{ backgroundImage: `url(${bg})` }}
-        className="relative mt-[-8px] h-[460px] bg-cover bg-fixed bg-center"
+        className="relative -mt-2 h-[460px] bg-cover bg-fixed bg-center"
       >
         <div className="absolute inset-0 bg-black opacity-50"></div>
         <div className="absolute inset-0 flex flex-col px-16 py-8 text-slate-50">
           <div className="flex w-full justify-between">
             <div className="text-2xl font-semibold">WanderLust</div>
             <button
-              className="rounded border border-slate-50 px-4 py-2"
+              className="rounded border border-slate-50 px-4 py-2 hover:bg-slate-500/25"
               onClick={() => {
                 navigate('/add-blog');
               }}
@@ -40,23 +41,23 @@ function HomePage() {
             </button>
           </div>
           <div className="flex max-w-5xl flex-1 flex-col justify-end pb-8 text-slate-50">
-            <h1 className="text-4xl font-bold">Journey Beyond Horizons</h1>
+            <h1 className="text-4xl font-bold tracking-wide">Journey Beyond Horizons</h1>
             <p className="my-4 text-xl">
-              Dive into the World of Travel with Stories That Transport You to Far-Off Lands.
+              Dive into the world of travel with stories that transport you to far-off lands.
               Adventure awaits around every corner. It's time to explore the world!
             </p>
-            <div className="text-xl font-semibold">Let's Go !!!</div>
+            <div className="text-xl font-semibold tracking-wide">Let's go!</div>
           </div>
         </div>
       </div>
-      <div className="px-16">
+      <div className="mx-16">
         <BlogFeed />
         <h1 className="text-2xl font-semibold dark:text-slate-50">All posts</h1>
         <div className="-mx-4 flex flex-wrap">
           {posts.length === 0
             ? Array(8)
-                .fill(0)
-                .map((_, index) => <PostCardSkeleton key={index} />)
+              .fill(0)
+              .map((_, index) => <PostCardSkeleton key={index} />)
             : posts.map((post) => <PostCard key={post._id} post={post} />)}
         </div>
       </div>
