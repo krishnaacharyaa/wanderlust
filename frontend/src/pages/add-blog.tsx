e2ee667db6e5586be00da1808d5bb97f2cb44ac4
Chinmay Kunkikar <chinmay.kunkikar@gmail.com>
2023-12-01 22:38:27 +0530

    styles: Change some styles of the submit form again

--- frontend/src/pages/add-blog.tsx
+++ frontend/src/pages/add-blog.tsx
@@ -123,7 +123,7 @@ function AddBlog() {
   }, []);
 
   function Asterisk() {
-    return <span className='dark:text-dark-tertiary'>*</span>
+    return <span className="dark:text-dark-tertiary">*</span>;
   }
 
   return (
@@ -142,46 +142,70 @@ function AddBlog() {
       </div>
       <div className="flex justify-center">
         <form onSubmit={handleSubmit} className="md:w-2/3">
+          <div className="mb-2 flex items-center">
+            <label className="flex items-center">
+              <span className="px-2 text-base font-medium text-light-secondary dark:text-dark-secondary">
+                Is this a featured blog?
+              </span>
+              <input
+                type="checkbox"
+                name="isFeaturedPost"
+                className="ml-2 h-5 w-5 rounded-full accent-purple-400 "
+                checked={formData.isFeaturedPost}
+                onChange={handleCheckboxChange}
+              />
+            </label>
+          </div>
+
           <div className="mb-2">
-            <div className="px-2 py-1 text-dark-secondary">Blog title <Asterisk /></div>
+            <div className="px-2 py-1 font-medium text-light-secondary dark:text-dark-secondary">
+              Blog title <Asterisk />
+            </div>
             <input
               type="text"
               name="title"
               placeholder="Travel Bucket List for this Year"
-              autoComplete='off'
-              className="w-full rounded-lg bg-slate-50 p-3 placeholder:text-sm placeholder:text-slate-800 dark:bg-dark-card dark:text-slate-50 dark:placeholder:text-dark-tertiary"
+              autoComplete="off"
+              className="w-full rounded-lg bg-slate-200 p-3 placeholder:text-sm placeholder:text-light-tertiary dark:bg-dark-card dark:text-slate-50 dark:placeholder:text-dark-tertiary"
               value={formData.title}
               onChange={handleInputChange}
             />
           </div>
 
           <div className="mb-1">
-            <div className="px-2 py-1 text-dark-secondary">Blog content <Asterisk /></div>
+            <div className="px-2 py-1 font-medium text-light-secondary dark:text-dark-secondary">
+              Blog content <Asterisk />
+            </div>
             <textarea
               name="description"
               placeholder="Start writing here&hellip;"
               rows={5}
-              className="w-full rounded-lg bg-slate-50 p-3 placeholder:text-sm placeholder:text-slate-800 dark:bg-dark-card dark:text-slate-50 dark:placeholder:text-dark-tertiary"
+              className="w-full rounded-lg bg-slate-200 p-3 placeholder:text-sm placeholder:text-light-tertiary dark:bg-dark-card dark:text-slate-50 dark:placeholder:text-dark-tertiary"
               value={formData.description}
               onChange={handleInputChange}
             />
           </div>
 
           <div className="mb-2">
-            <div className="px-2 py-1 text-dark-secondary">Author name <Asterisk /></div>
+            <div className="px-2 py-1 font-medium text-light-secondary dark:text-dark-secondary">
+              Author name <Asterisk />
+            </div>
             <input
               type="text"
               name="authorName"
               placeholder="Shree Sharma"
-              className="w-full rounded-lg bg-slate-50 p-3 placeholder:text-sm placeholder:text-slate-800 dark:bg-dark-card dark:text-slate-50 dark:placeholder:text-dark-tertiary"
+              className="w-full rounded-lg bg-slate-200 p-3 placeholder:text-sm placeholder:text-light-tertiary dark:bg-dark-card dark:text-slate-50 dark:placeholder:text-dark-tertiary"
               value={formData.authorName}
               onChange={handleInputChange}
             />
           </div>
 
-          <div className="px-2 py-1 text-dark-secondary">
+          <div className="px-2 py-1 font-medium text-light-secondary dark:text-dark-secondary">
             Blog cover image
-            <span className="text-xs tracking-wide text-dark-tertiary">&nbsp;(jpg/png/webp)&nbsp;</span><Asterisk />
+            <span className="text-xs tracking-wide text-dark-tertiary">
+              &nbsp;(jpg/png/webp)&nbsp;
+            </span>
+            <Asterisk />
           </div>
           <div className="mb-4 flex justify-between gap-4">
             <div className="w-10/12">
@@ -190,15 +214,15 @@ function AddBlog() {
                 id="imagelink"
                 name="imageLink"
                 placeholder="https://&hellip;"
-                autoComplete='off'
-                className="w-full rounded-lg bg-slate-50 p-3 placeholder:text-sm placeholder:text-slate-800 dark:bg-dark-card dark:text-slate-50 dark:placeholder:text-dark-tertiary"
+                autoComplete="off"
+                className="w-full rounded-lg bg-slate-200 p-3 placeholder:text-sm placeholder:text-light-tertiary dark:bg-dark-card dark:text-slate-50 dark:placeholder:text-dark-tertiary"
                 value={formData.imageLink}
                 onChange={handleInputChange}
               />
             </div>
             <button
               type="button"
-              className="w-2/12 rounded-lg bg-black px-4 py-3 text-slate-50 hover:bg-slate-800 dark:bg-dark-primary dark:text-dark-card dark:hover:bg-dark-secondary"
+              className="w-2/12 rounded-lg bg-light-primary px-4 py-3 text-slate-50 hover:bg-light-primary/80 dark:bg-dark-primary dark:text-dark-card dark:hover:bg-dark-secondary/80"
               onClick={() => {
                 setmodal(true);
               }}
@@ -207,18 +231,19 @@ function AddBlog() {
             </button>
           </div>
           <div className="mb-4 flex flex-col">
-            <label className="dark:text-dark-secondary md:mr-4 md:w-fit px-2 pb-1">
+            <label className="px-2 pb-1 font-medium text-light-secondary dark:text-dark-secondary md:mr-4 md:w-fit">
               Categories <Asterisk />
             </label>
-            <div className="flex flex-wrap gap-3 dark:bg-dark-card dark:p-3 dark:rounded-lg">
+            <div className="flex flex-wrap gap-3 rounded-lg p-2 dark:bg-dark-card dark:p-3">
               {CATEGORIES.map((category) => (
                 <span
                   key={category}
                   className={`cursor-pointer
-									${formData.categories.includes(category)
+									${
+                    formData.categories.includes(category)
                       ? categoryProps(category, true)
                       : categoryProps(category, false)
-                    }`}
+                  }`}
                   onClick={() => handleCategoryClick(category)}
                 >
                   {category}
@@ -227,24 +252,9 @@ function AddBlog() {
             </div>
           </div>
 
-          <div className="mb-4 flex items-center">
-            <label className="flex items-center">
-              <span className="text-base font-medium text-slate-800 dark:text-dark-secondary px-1">
-                Is this a featured blog?
-              </span>
-              <input
-                type="checkbox"
-                name="isFeaturedPost"
-                className="ml-2 h-4 w-4 rounded-full border"
-                checked={formData.isFeaturedPost}
-                onChange={handleCheckboxChange}
-              />
-            </label>
-          </div>
-
           <button
             type="submit"
-            className="flex w-full items-center justify-center rounded-lg bg-black px-5 md:mx-1 py-3 text-base font-semibold text-white hover:bg-slate-800 dark:bg-dark-primary dark:text-dark-card dark:hover:bg-dark-secondary md:w-fit"
+            className="flex w-full items-center justify-center rounded-lg bg-light-primary px-12 py-3 text-base font-semibold text-light hover:bg-light-primary/80 dark:bg-dark-primary dark:text-dark-card dark:hover:bg-dark-secondary/80 md:mx-1 md:w-fit"
           >
             Post blog
           </button>
