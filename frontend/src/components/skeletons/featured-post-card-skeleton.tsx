dbfeaf7777c8d41c2a985f1e156ac866ec280f94
Chinmay Kunkikar <chinmay.kunkikar@gmail.com>
2023-12-01 19:07:38 +0530

    feat: Add skeletons for dark mode (fixes #64)
    - Remove unnecessary classes

--- frontend/src/components/skeletons/featured-post-card-skeleton.tsx
+++ frontend/src/components/skeletons/featured-post-card-skeleton.tsx
@@ -2,26 +2,33 @@ import { Skeleton } from '@/components/ui/skeleton';
 
 export const FeaturedPostCardSkeleton = () => {
   return (
-    <div className="flex h-48 gap-4 rounded-lg bg-slate-50">
+    <div className="bg-light flex rounded-lg dark:bg-dark-card">
+      {/* for image */}
       <div className="w-1/3">
-        {/* for image */}
-        <Skeleton className="h-full w-full rounded-lg bg-slate-200" />
+        <Skeleton className="h-full w-full rounded-lg bg-slate-200 dark:bg-slate-700" />
       </div>
-      <div className="flex h-full w-2/3 flex-col gap-2">
-        {/* for title */}
-        <Skeleton className="h-6 w-full bg-slate-200 text-xl" />
-        <Skeleton className="h-6 w-2/3 bg-slate-200 text-xl" />
-        <div className="flex flex-wrap gap-2">
-          {/* for categories */}
-          <Skeleton className="h-6 w-16 rounded-3xl bg-slate-200" />
-          <Skeleton className="h-6 w-16 rounded-3xl bg-slate-200" />
-          <Skeleton className="h-6 w-16 rounded-3xl bg-slate-200" />
-        </div>
-        {/* for description */}
-        <Skeleton className="line-clamp-2 h-12 w-full bg-slate-200" />
-        <div className="mb-1 flex flex-1 items-end">
-          {/* for author and time */}
-          <Skeleton className="h-5 w-1/3 bg-slate-200" />
+      <div className="flex h-48 w-2/3 gap-4 rounded-lg p-3">
+        <div className="flex w-full flex-col gap-3">
+          {/* for title */}
+          <Skeleton className="h-6 w-11/12 bg-slate-200 dark:bg-slate-700" />
+          <Skeleton className="h-6 w-2/3 bg-slate-200 dark:bg-slate-700" />
+          <div className="flex flex-wrap gap-2">
+            {/* for categories */}
+            {Array(3)
+              .fill(0)
+              .map((_, index) => (
+                <Skeleton
+                  key={index}
+                  className="h-6 w-16 rounded-full bg-slate-200 dark:bg-slate-700"
+                />
+              ))}
+          </div>
+          {/* for description */}
+          <Skeleton className="line-clamp-2 h-12 w-10/12 bg-slate-200 dark:bg-slate-700" />
+          <div className="mb-1 flex flex-1 items-end">
+            {/* for author and time */}
+            <Skeleton className="h-3 w-1/3 bg-slate-200 dark:bg-slate-700" />
+          </div>
         </div>
       </div>
     </div>
