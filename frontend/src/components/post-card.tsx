17b2c4efc849f73fc50a57997990d4f66d29a342
Chinmay Kunkikar <chinmay.kunkikar@gmail.com>
2023-12-01 17:27:29 +0530

    style: Use original breakpoint widths for cards

--- frontend/src/components/post-card.tsx
+++ frontend/src/components/post-card.tsx
@@ -10,7 +10,7 @@ export default function PostCard({ post }: { post: Post }) {
   }
   const slug = createSlug(post.title);
   return (
-    <div className="w-full sm:w-1/2 lg:w-1/4 xl:w-1/5">
+    <div className="w-full md:w-1/2 lg:w-1/3 xl:w-1/4">
       <div
         className="bg-light m-4 cursor-pointer rounded-lg shadow-md dark:bg-dark-card"
       onClick={() => navigate(`/details-page/${slug}/${post._id}`, { state: { post } })}
