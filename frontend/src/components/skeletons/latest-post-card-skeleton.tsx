ee7b866d9fecc6696e55a598b5dd6d9b12d279dc
Chinmay Kunkikar <chinmay.kunkikar@gmail.com>
2023-12-01 19:07:38 +0530

    feat: Add skeletons for dark mode (fixes #64)
    - Remove unnecessary classes

--- frontend/src/components/skeletons/latest-post-card-skeleton.tsx
+++ frontend/src/components/skeletons/latest-post-card-skeleton.tsx
@@ -2,22 +2,24 @@ import { Skeleton } from '@/components/ui/skeleton';
 
 export const LatestPostCardSkeleton = () => {
   return (
-    <div className="rounded-lg bg-slate-50 py-2 shadow-sm">
+    <div className="bg-light rounded-lg p-3 dark:bg-dark-card">
       <div className="flex">
         <div className="mb-2 flex flex-1 flex-wrap gap-2">
-          <Skeleton className="h-6 w-16 rounded-3xl bg-slate-200" />
-          <Skeleton className="h-6 w-16 rounded-3xl bg-slate-200" />
-          <Skeleton className="h-6 w-16 rounded-3xl bg-slate-200" />
+          {Array(3)
+            .fill(0)
+            .map((_, index) => (
+              <Skeleton
+                key={index}
+                className="h-6 w-16 rounded-full bg-slate-200 dark:bg-slate-700"
+              />
+            ))}
         </div>
-        <Skeleton className="h-4 w-4 bg-slate-200" style={{ height: 12, width: 12 }} />
       </div>
-      <div className="mb-2 line-clamp-2 text-xl font-semibold">
-        <Skeleton className="h-6 w-full bg-slate-200" />
-        <Skeleton className="mt-2 h-6 w-2/3 bg-slate-200" />
+      <div className="mb-2">
+        <Skeleton className="h-6 w-11/12 bg-slate-200 dark:bg-slate-700" />
+        <Skeleton className="mt-2 h-4 w-2/3 bg-slate-200 dark:bg-slate-700" />
       </div>
-      <div className="flex text-xs text-slate-500">
-        <Skeleton className="h-4 w-1/3 bg-slate-200" />
-      </div>
+      <Skeleton className="h-3 w-1/3 bg-slate-200 dark:bg-slate-700" />
     </div>
   );
 };
--- frontend/src/components/skeletons/post-card-skeleton.tsx
+++ frontend/src/components/skeletons/post-card-skeleton.tsx
@@ -2,18 +2,16 @@ import { Skeleton } from '@/components/ui/skeleton';
 
 export const PostCardSkeleton = () => {
   return (
-    <div className="w-full p-4 md:w-1/2 lg:w-1/3 xl:w-1/4">
-      <div className="rounded-lg bg-slate-50 shadow-md">
-        <Skeleton className="h-48 w-full rounded-lg" />
+    <div className="w-full md:w-1/2 lg:w-1/3 xl:w-1/4">
+      <div className="bg-light m-4 rounded-lg shadow-md dark:bg-dark-card">
+        <Skeleton className="h-48 w-full rounded-lg bg-slate-200 dark:bg-slate-700" />
         <div className="p-4">
-          <div className="mb-2 flex text-xs text-slate-500">
-            <Skeleton className="mr-2 h-4 w-2/3 bg-slate-200" />
-          </div>
-          <Skeleton className="mb-2 h-6 w-4/5 bg-slate-200" />
-          <Skeleton className="line-clamp-2 h-14 w-full bg-slate-200 text-slate-600" />
-          <div className="mt-4 flex flex-wrap gap-2">
-            <Skeleton className="h-6 w-16 rounded-3xl bg-slate-200" />
-            <Skeleton className="h-6 w-16 rounded-3xl bg-slate-200" />
+          <Skeleton className="mb-2 h-3 w-2/3 bg-slate-200 dark:bg-slate-700" />
+          <Skeleton className="mb-2 h-6 w-4/5 bg-slate-200 dark:bg-slate-700" />
+          <Skeleton className="h-16 w-11/12 bg-slate-200 dark:bg-slate-700" />
+          <div className="mt-2 flex flex-wrap gap-2">
+            <Skeleton className="h-6 w-16 rounded-full bg-slate-200 dark:bg-slate-700" />
+            <Skeleton className="h-6 w-16 rounded-full bg-slate-200 dark:bg-slate-700" />
           </div>
         </div>
       </div>
