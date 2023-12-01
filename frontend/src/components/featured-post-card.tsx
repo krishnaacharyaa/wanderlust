ea05ef30edb45798ba4fe4ea9df7ce2a88f3e95d
Chinmay Kunkikar <chinmay.kunkikar@gmail.com>
2023-12-01 17:18:55 +0530

    feat: Add a `light` theme to tailwind config Change light theme colors according to the theme vars

--- frontend/src/components/featured-post-card.tsx
+++ frontend/src/components/featured-post-card.tsx
@@ -7,7 +7,7 @@ export default function FeaturedPostCard({ post }: { post: Post }) {
   const navigate = useNavigate();
   return (
     <div
-      className="flex h-48 cursor-pointer gap-2 rounded-lg bg-slate-50 dark:bg-dark-card"
+      className="flex h-48 cursor-pointer gap-2 rounded-lg bg-light dark:bg-dark-card"
       onClick={() => navigate('/details-page', { state: { post } })}
     >
       <div className="w-1/3">
@@ -18,7 +18,7 @@ export default function FeaturedPostCard({ post }: { post: Post }) {
         />
       </div>
       <div className="flex h-full w-2/3 flex-col gap-2 p-2">
-        <div className="text-xl font-semibold dark:text-dark-title">{post.title}</div>
+        <div className="text-xl font-semibold text-light-title dark:text-dark-title">{post.title}</div>
         <div className="flex flex-wrap gap-2">
           {post.categories.map((category, index) => (
             <span key={index} className={categoryProps(category)}>
@@ -26,10 +26,10 @@ export default function FeaturedPostCard({ post }: { post: Post }) {
             </span>
           ))}
         </div>
-        <div className="line-clamp-2 text-slate-700">
-          <p className="overflow-ellipsis dark:text-dark-description">{post.description}</p>
+        <div className="line-clamp-2">
+          <p className="overflow-ellipsis text-light-description dark:text-dark-description">{post.description}</p>
         </div>
-        <div className="flex flex-1 items-end text-xs text-slate-600 dark:text-dark-info">
+        <div className="flex flex-1 items-end text-xs text-light-info dark:text-dark-info">
           {post.authorName} • {formatPostTime(post.timeOfPost)}
         </div>
       </div>
--- frontend/src/components/post-card.tsx
+++ frontend/src/components/post-card.tsx
@@ -7,23 +7,23 @@ export default function PostCard({ post }: { post: Post }) {
   const navigate = useNavigate();
   return (
     <div className="w-full sm:w-1/2 lg:w-1/4 xl:w-1/5">
-    <div
+      <div
         className="bg-light m-4 cursor-pointer rounded-lg shadow-md dark:bg-dark-card"
-      onClick={() => navigate('/details-page', { state: { post } })}
-    >
+        onClick={() => navigate('/details-page', { state: { post } })}
+      >
         <img
           src={post.imageLink}
           alt={post.title}
           className="h-48 w-full rounded-lg object-cover"
         />
         <div className="p-4">
-          <div className="mb-2 text-xs text-slate-500 dark:text-slate-400">
+          <div className="text-light-info mb-2 text-xs dark:text-dark-info">
             {post.authorName} • {formatPostTime(post.timeOfPost)}
           </div>
-          <h2 className="mb-2 line-clamp-1 text-xl font-semibold dark:text-slate-50">
+          <h2 className="text-light-title mb-2 line-clamp-1 text-xl font-semibold dark:text-dark-title">
             {post.title}
           </h2>
-          <p className="line-clamp-2 text-slate-600 dark:text-dark-description">
+          <p className="text-light-description line-clamp-2 dark:text-dark-description">
             {post.description}
           </p>
           <div className="mt-4 flex flex-wrap gap-2">
