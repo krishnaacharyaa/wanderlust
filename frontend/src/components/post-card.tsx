56df01c6d3f97086ec2ccab2b3dcdcd679fb1fd6
Chinmay Kunkikar <chinmay.kunkikar@gmail.com>
2023-12-01 17:27:29 +0530

    style: Use original breakpoint widths for cards

--- frontend/src/components/post-card.tsx
+++ frontend/src/components/post-card.tsx
@@ -6,7 +6,7 @@ import { categoryProps } from '@/utils/category-props';
 export default function PostCard({ post }: { post: Post }) {
   const navigate = useNavigate();
   return (
-    <div className="w-full sm:w-1/2 lg:w-1/4 xl:w-1/5">
+    <div className="w-full md:w-1/2 lg:w-1/3 xl:w-1/4">
       <div
         className="bg-light m-4 cursor-pointer rounded-lg shadow-md dark:bg-dark-card"
         onClick={() => navigate('/details-page', { state: { post } })}
