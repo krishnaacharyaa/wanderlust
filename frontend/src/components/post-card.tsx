7f8fc8a25e4b34ef17e8573392bda3e66e672c2f
Chinmay Kunkikar <chinmay.kunkikar@gmail.com>
2023-12-01 21:42:04 +0530

    styles: Add some visual hierarchy by adjusting text sizes.

--- frontend/src/components/post-card.tsx
+++ frontend/src/components/post-card.tsx
@@ -8,22 +8,22 @@ export default function PostCard({ post }: { post: Post }) {
   return (
     <div className="w-full md:w-1/2 lg:w-1/3 xl:w-1/4">
       <div
-        className="bg-light m-4 cursor-pointer rounded-lg shadow-md dark:bg-dark-card"
+        className="m-4 cursor-pointer rounded-lg bg-light shadow-md dark:bg-dark-card"
         onClick={() => navigate('/details-page', { state: { post } })}
       >
         <img
           src={post.imageLink}
           alt={post.title}
-          className="h-48 w-full rounded-lg object-cover"
+          className="h-48 w-full rounded-t-lg object-cover"
         />
         <div className="p-4">
-          <div className="text-light-info mb-2 text-xs dark:text-dark-info">
+          <div className="mb-1 text-xs text-light-info dark:text-dark-info">
             {post.authorName} • {formatPostTime(post.timeOfPost)}
           </div>
-          <h2 className="text-light-title mb-2 line-clamp-1 text-xl font-semibold dark:text-dark-title">
+          <h2 className="mb-2 line-clamp-1 text-lg font-semibold text-light-title dark:text-dark-title">
             {post.title}
           </h2>
-          <p className="text-light-description line-clamp-2 dark:text-dark-description">
+          <p className="line-clamp-2 text-sm text-light-description dark:text-dark-description">
             {post.description}
           </p>
           <div className="mt-4 flex flex-wrap gap-2">
