f1390c2a8948fe1c596b75a65632bbc66e8e7f85
lkanwat <lkanwat@hotmail.com>
2023-12-15 20:18:16 +0530

    tag-#77: correct the title word length issue and add new msg according to the authorName lenght

--- frontend/src/lib/blog.zod.ts
+++ frontend/src/lib/blog.zod.ts
@@ -1,12 +1,30 @@
 import { z } from 'zod';
 
+// validate the image link
+const isValidImageLink = (value: string) => {
+  const imageLinkRegex = /\.(jpg|jpeg|png|webp)$/i;
+  return imageLinkRegex.test(value);
+};
+
+const isValidAuthorName = (value: string) => {
+  const trimmedLength = value.trim().length;
+
+  // Add conditions to check the length
+  return trimmedLength >= 3 && trimmedLength <= 15;
+};
+
 export const FormDataSchema = z.object({
-  title: z.string().refine((value) => value.trim().split(/\s+/).length > 3, {
+  title: z.string().refine((value) => value.trim().split(/\s+/).length >= 3, {
     message: 'Oops! Title needs more spice. Give it at least 3 words.',
   }),
-  authorName: z.string().refine((value) => value.trim().length >= 3 && value.trim().length <= 15, {
-    message: 'Hey, Your name should be more than 5 letters.',
-  }),
+  authorName: z
+    .string()
+    .refine((value) => value.length >= 3, {
+      message: "C'ome on! Your name cannot be less than 3 characters.",
+    })
+    .refine((value) => value.length <= 15, {
+      message: "Hey isn't it too big of a name, can you limit it to 15 characters",
+    }),
   imageLink: z.string().refine((value) => isValidImageLink(value), {
     message: 'Hmm... Image link should end with .jpg, .jpeg, .webp, or .png.',
   }),
@@ -16,11 +34,5 @@ export const FormDataSchema = z.object({
   description: z.string(),
   isFeaturedPost: z.boolean(),
 });
-
-// validate the image link
-const isValidImageLink = (value: string) => {
-  const imageLinkRegex = /\.(jpg|jpeg|png|webp)$/i;
-  return imageLinkRegex.test(value);
-};
 
 export type TFormData = z.infer<typeof FormDataSchema>;
