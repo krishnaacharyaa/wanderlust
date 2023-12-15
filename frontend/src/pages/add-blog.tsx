d24d7e66a232c3fc03727414a4895adb3f75c958
lkanwat <lkanwat@hotmail.com>
2023-12-15 19:18:12 +0530

    tag-#77: try to solve versel conflict

--- frontend/src/pages/add-blog.tsx
+++ frontend/src/pages/add-blog.tsx
@@ -1,6 +1,6 @@
 import { useForm } from 'react-hook-form';
 import axios from 'axios';
-import { ChangeEvent, FormEvent, useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
