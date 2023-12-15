2944202c812668cffd6873a098fd9603e830f5a9
shubham bajaj <shmbajaj32@gmail.com>
2023-12-15 21:21:27 +0530

    made jest undetstand absolute paths

--- frontend/jest.config.ts
+++ frontend/jest.config.ts
@@ -7,8 +7,12 @@ module.exports = {
     // process `*.tsx` files with `ts-jest`
   },
   moduleNameMapper: {
+    // mocking assests and styling
     '\\.(gif|ttf|eot|svg|png|webp)$': '<rootDir>/config/jest/fileMock.ts',
     '^.+\\.(css|less|scss|sass)$': '<rootDir>/config/jest/styleMock.ts',
+
+    // making jest understand absolute paths
+    '@/(.*)$': '<rootDir>/$1',
   },
   setupFilesAfterEnv: ['./config/jest/setupTests.ts'],
   moduleFileExtensions: [
