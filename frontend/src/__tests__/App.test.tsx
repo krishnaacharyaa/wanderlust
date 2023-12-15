291b997dfc266aed861340a2e9d0e996d0b54901
shubham bajaj <shmbajaj32@gmail.com>
2023-12-15 21:21:27 +0530

    made jest undetstand absolute paths

--- frontend/src/__tests__/App.test.tsx
+++ frontend/src/__tests__/App.test.tsx
@@ -1,5 +1,8 @@
+import { render } from '@testing-library/react';
+import App from '../App';
+
 describe('App', () => {
   it('should work as expected', () => {
-    expect(1 + 1).toBe(2);
+    render(<App />);
   });
 });
