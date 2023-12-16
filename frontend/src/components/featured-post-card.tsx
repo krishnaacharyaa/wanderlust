efd899cbb5bfbd0d84fe6c9d21c71cc4d955499b
geetanjalichawla <geetc1919@gmail.com>
2023-12-17 01:47:38 +0530

    fix: Removed ref hooks from the last changes

--- frontend/src/components/featured-post-card.tsx
+++ frontend/src/components/featured-post-card.tsx
@@ -3,21 +3,10 @@ import Post from '@/types/post-type';
 import formatPostTime from '@/utils/format-post-time';
 import CategoryPill from '@/components/category-pill';
 import { createSlug } from '@/utils/slug-generator';
-import { useEffect, useRef, useState } from 'react';
 
 export default function FeaturedPostCard({ post }: { post: Post }) {
   const navigate = useNavigate();
   const slug = createSlug(post.title);
-  const titleRef = useRef(null);
-
-  const [cardHeight, setCardHeight] = useState(28);
-
-  useEffect(() => {
-    const titleElement = titleRef.current as unknown as HTMLDivElement;
-    const titleHeight = titleElement?.offsetHeight;
-    console.log('Title Height:', titleHeight);
-    setCardHeight(titleHeight);
-  }, [titleRef]); 
 
   return (
     <div
@@ -32,7 +21,7 @@ export default function FeaturedPostCard({ post }: { post: Post }) {
         />
       </div>
       <div className="flex h-full w-full md:w-1/7 flex-col gap-2 p-2">
-        <div ref={titleRef} className="text-xl font-semibold text-light-title dark:text-dark-title max-sm:line-clamp-2">
+        <div className="text-xl font-semibold text-light-title dark:text-dark-title max-sm:line-clamp-2">
           {post.title}
         </div>
         <div className="flex flex-wrap gap-2">
@@ -40,8 +29,8 @@ export default function FeaturedPostCard({ post }: { post: Post }) {
             <CategoryPill key={`${category}-${index}`} category={category} />
           ))}
         </div>
-        <div className={ cardHeight >28?`line-clamp-4 md:line-clamp-2`: `line-clamp-5 md:line-clamp-2`}>
-          <p className="overflow-ellipsis text-sm text-light-description dark:text-dark-description">
+        <div className={ `line-clamp-2`}>
+          <p className="overflow-ellipsis max-sm:text-sm text-light-description dark:text-dark-description">
             {post.description}
           </p>
         </div>
