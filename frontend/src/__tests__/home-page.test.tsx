ca8ef6712a0e8d573a60c193119bb5328890e71b
shubham bajaj <shmbajaj32@gmail.com>
2024-01-01 20:09:23 +0530

    chore: test skeleton added

---
+++ frontend/src/__tests__/home-page.test.tsx
@@ -0,0 +1,10 @@
+describe('Integration Test: Home Route', () => {
+  test('displays home page', () => {});
+  test('on create post click navigates to /add-blog page', () => {});
+  test('displays BlogFeed', () => {});
+  test('on featured post click navigates to /details-page/:title/:id page', () => {});
+  test('failed to display BlogFeed', () => {});
+  test('displays all post', () => {});
+  test('on all-post post click navigates to /details-page/:title/:id page', () => {});
+  test('failed to display all post', () => {});
+});
