162bd40bbae7aba651372aff97dfff01c1b068c1
SohamRoy-01 <sohamroy.bittu1234@gmail.com>
2024-03-25 18:49:12 +0530

    fix the inline issue of skeleton ui across all devices

--- frontend/src/components/skeletons/featured-post-card-skeleton.tsx
+++ frontend/src/components/skeletons/featured-post-card-skeleton.tsx
@@ -1,37 +1,36 @@
-import { Skeleton } from '@/components/ui/skeleton';
+import { Skeleton } from "../ui/skeleton";
 
 export const FeaturedPostCardSkeleton = () => {
   return (
-    <div
-      className="flex rounded-lg bg-light dark:bg-dark-card"
-      data-testid="featurepostcardskeleton"
-    >
-      {/* for image */}
-      <div className="w-full sm:w-1/3">
+    <div className="flex flex-col sm:flex-row rounded-lg bg-light dark:bg-dark-card" data-testid="featurepostcardskeleton">
+      {/* Image */}
+      <div className="w-full sm:w-1/3 mb-4 sm:mb-0">
         <Skeleton className="h-48 sm:h-full w-full rounded-lg bg-slate-200 dark:bg-slate-700" />
       </div>
-      <div className="flex flex-col sm:w-2/3 gap-4 rounded-lg p-3">
-        <div className="flex w-full flex-col gap-3">
-          {/* for title */}
-          <Skeleton className="h-6 w-full sm:w-11/12 bg-slate-200 dark:bg-slate-700" />
-          <Skeleton className="h-6 w-full sm:w-2/3 bg-slate-200 dark:bg-slate-700" />
-          <div className="flex flex-wrap gap-2">
-            {/* for categories */}
-            {Array(3)
-              .fill(0)
-              .map((_, index) => (
-                <Skeleton
-                  key={index}
-                  className="h-6 w-full sm:w-16 rounded-full bg-slate-200 dark:bg-slate-700"
-                />
-              ))}
-          </div>
-          {/* for description */}
-          <Skeleton className="line-clamp-2 h-12 w-full sm:w-10/12 bg-slate-200 dark:bg-slate-700" />
-          <div className="mb-1 flex flex-1 items-end">
-            {/* for author and time */}
-            <Skeleton className="h-3 w-full sm:w-1/3 bg-slate-200 dark:bg-slate-700" />
-          </div>
+
+      {/* Content */}
+      <div className="flex flex-col sm:w-2/3 p-3">
+        {/* Title */}
+        <Skeleton className="h-6 w-full mb-2 bg-slate-200 dark:bg-slate-700" />
+        
+        {/* Categories */}
+        <div className="flex flex-wrap gap-2 mb-2">
+          {Array(3)
+            .fill(0)
+            .map((_, index) => (
+              <Skeleton
+                key={index}
+                className="h-6 w-16 rounded-full bg-slate-200 dark:bg-slate-700"
+              />
+            ))}
+        </div>
+        
+        {/* Description */}
+        <Skeleton className="h-12 w-full mb-2 bg-slate-200 dark:bg-slate-700" />
+        
+        {/* Author and Time */}
+        <div className="flex items-end">
+          <Skeleton className="h-3 w-1/3 bg-slate-200 dark:bg-slate-700" />
         </div>
       </div>
     </div>
