e5a726acbbc994f89512f58f42bb0b814ad90c04
Mohitsen11 <mohitsen0102@gmail.com>
2024-03-30 14:03:16 +0530

    fix-#107: Added children prop to bind other components with footer-layout

--- frontend/src/layouts/footer-layout.tsx
+++ frontend/src/layouts/footer-layout.tsx
@@ -1,16 +1,19 @@
-function footer() {
+function footer({ children }: any) {
   const newDate = new Date();
   const year = newDate.getFullYear();
 
   return (
-      <footer className="relative left-0 bottom-0 h-[10vh] flex flex-col sm:flex-row items-center justify-between py-5 sm:px-20 text-white bg-[#181818]">
-          <section className="text-lg">
+      <>
+        {children}
+      <footer className="relative left-0 bottom-0 sm:h-[10vh] flex flex-col sm:flex-row items-center justify-between py-3 sm:py-5 px-5 sm:px-20 text-white bg-[#181818]">
+          <section className="text-lg mb-2 sm:mb-0">
                 WonderLust
           </section>
           <section className="text-lg">
                 © {year} All Rights Reserved
           </section>
       </footer>
+      </>
   );
 }
 
