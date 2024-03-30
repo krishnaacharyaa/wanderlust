b613be97efc444227caaa6131a0b3630813042ae
Mohitsen11 <mohitsen0102@gmail.com>
2024-03-30 20:16:53 +0530

    fix-#107: Updated the css to make it  mobile responsive

--- frontend/src/layouts/footer-layout.tsx
+++ frontend/src/layouts/footer-layout.tsx
@@ -3,7 +3,7 @@ function footer() {
   const year = newDate.getFullYear();
 
   return (
-      <>
+      <div className="w-full text-center self-end">
       <footer className="relative left-0 bottom-0 sm:h-[8vh] flex flex-col sm:flex-row items-center justify-between sm:py-3 sm:px-5 md:px-8 lg:px-20 text-white bg-[#181818]">
           <section className="text-lg">
                 WonderLust
@@ -12,7 +12,7 @@ function footer() {
                 © {year} All Rights Reserved
           </section>
       </footer>
-      </>
+      </div>
   );
 }
 
