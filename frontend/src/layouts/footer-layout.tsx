87e6f1efc03b8c7500944b37c68a428e46382eca
Mohitsen11 <mohitsen0102@gmail.com>
2024-03-30 16:19:33 +0530

    fix-#107: Added footer-layout in the parent component to avoid the repeatation

--- frontend/src/layouts/footer-layout.tsx
+++ frontend/src/layouts/footer-layout.tsx
@@ -1,10 +1,9 @@
-function footer({ children }: any) {
+function footer() {
   const newDate = new Date();
   const year = newDate.getFullYear();
 
   return (
       <>
-        {children}
       <footer className="relative left-0 bottom-0 sm:h-[10vh] flex flex-col sm:flex-row items-center justify-between py-3 sm:py-5 px-5 sm:px-20 text-white bg-[#181818]">
           <section className="text-lg mb-2 sm:mb-0">
                 WonderLust
