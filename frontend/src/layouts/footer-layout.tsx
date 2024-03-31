7f0b04ab66740a77b2355ab34c36241325a72662
Mohitsen11 <mohitsen0102@gmail.com>
2024-03-31 11:34:26 +0530

    fix-#107: fixed typo + Correction in alignment + reduced footer height

--- frontend/src/layouts/footer-layout.tsx
+++ frontend/src/layouts/footer-layout.tsx
@@ -3,16 +3,14 @@ function footer() {
   const year = newDate.getFullYear();
 
   return (
-      <div className="w-full text-center self-end">
-      <footer className="relative left-0 bottom-0 sm:h-[8vh] flex flex-col sm:flex-row items-center justify-between sm:py-3 sm:px-5 md:px-8 lg:px-20 text-white bg-[#181818]">
-          <section className="text-lg">
-                WonderLust
+      <footer className="w-full text-center self-end relative left-0 bottom-0 sm:h-[7vh] flex flex-col sm:flex-row items-center justify-between sm:px-4 md:px-[2rem] lg:px-[4rem] text-white bg-[#181818]">
+          <section className="text-[16px]">
+                WanderLust
           </section>
-          <section className="text-lg">
-                © {year} All Rights Reserved
+          <section className="text-[16px]">
+                <span className="mr-2">&copy;</span>{year} All Rights Reserved
           </section>
       </footer>
-      </div>
   );
 }
 
