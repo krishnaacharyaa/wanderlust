87a12b711de078fb2e32b20e644fb3e83f15c1ac
Mohitsen11 <mohitsen0102@gmail.com>
2024-03-31 11:34:26 +0530

    fix-#107: fixed typo + Correction in alignment + reduced footer height

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <BrowserRouter>
       <ScrollToTop />
-      <>
+      <div className='min-h-screen flex flex-col'>
       <Routes>
         <Route path="/">
           <Route index element={<HomePage />} />
@@ -17,8 +17,8 @@ function App() {
           <Route path="details-page/:title/:postId" element={<DetailsPage />} />
         </Route>
       </Routes>
-    <Footer/>
-    </>
+      <Footer/>
+      </div>
     </BrowserRouter>
   );
 }
