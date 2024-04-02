3fa247bd9337aa1c80f2ccf09dcf36829c1b96d4
Mohitsen11 <mohitsen0102@gmail.com>
2024-04-02 12:44:22 +0530

    refactor: npm run format command executed in frontend

--- frontend/src/layouts/footer-layout.tsx
+++ frontend/src/layouts/footer-layout.tsx
@@ -3,14 +3,13 @@ function footer() {
   const year = newDate.getFullYear();
 
   return (
-    <footer className="flex flex-col items-center sm:flex-row min-h-[6vh] sm:justify-between py-2 sm:py-3 bg-footer-background text-white text-center">
-          <section className="text-base sm:pl-4 md:pl-8 lg:pl-16">
-                WanderLust
-          </section>
-          <section className="text-base sm:pr-4 md:pr-16 lg:pr-24">
-                <span className="mr-2">&copy;</span>{year} All Rights Reserved
-          </section>
-      </footer>
+    <footer className="flex min-h-[6vh] flex-col items-center bg-footer-background py-2 text-center text-white sm:flex-row sm:justify-between sm:py-3">
+      <section className="text-base sm:pl-4 md:pl-8 lg:pl-16">WanderLust</section>
+      <section className="text-base sm:pr-4 md:pr-16 lg:pr-24">
+        <span className="mr-2">&copy;</span>
+        {year} All Rights Reserved
+      </section>
+    </footer>
   );
 }
 
--- frontend/src/layouts/header-layout.tsx
+++ frontend/src/layouts/header-layout.tsx
@@ -3,44 +3,40 @@ import AddIcon from '@/assets/svg/add-icon-white.svg';
 import { useNavigate } from 'react-router-dom';
 import Hero from '@/components/hero';
 function header() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    return (
-    <div
-        className="relative -mt-2 h-[460px] bg-cover bg-fixed bg-center bg-[url('./assets/wanderlustbg.webp')]"
-    >
-        <div className="absolute inset-0 bg-black opacity-50"></div>
-        <div className="absolute inset-0 flex flex-col px-4 py-8 text-slate-50 md:px-16">
+  return (
+    <div className="relative -mt-2 h-[460px] bg-[url('./assets/wanderlustbg.webp')] bg-cover bg-fixed bg-center">
+      <div className="absolute inset-0 bg-black opacity-50"></div>
+      <div className="absolute inset-0 flex flex-col px-4 py-8 text-slate-50 md:px-16">
         <div className="flex w-full justify-between">
-            <div className="flex items-center justify-between text-2xl font-semibold">
-                WanderLust
-            </div>
-            <div className="flex justify-between px-2">
+          <div className="flex items-center justify-between text-2xl font-semibold">WanderLust</div>
+          <div className="flex justify-between px-2">
             <div className="flex items-center justify-end px-2 py-2 md:px-20">
-                <ThemeToggle />
+              <ThemeToggle />
             </div>
             <button
-                className="hidden rounded border border-slate-50 px-4 py-2 hover:bg-slate-500/25 md:inline-block"
-                onClick={() => {
-                    navigate('/add-blog');
-                }}
+              className="hidden rounded border border-slate-50 px-4 py-2 hover:bg-slate-500/25 md:inline-block"
+              onClick={() => {
+                navigate('/add-blog');
+              }}
             >
-                Create post
+              Create post
             </button>
             <button
-                className="px-2 py-2 hover:bg-slate-500/25 md:hidden"
-                onClick={() => {
-                    navigate('/add-blog');
-                }}
+              className="px-2 py-2 hover:bg-slate-500/25 md:hidden"
+              onClick={() => {
+                navigate('/add-blog');
+              }}
             >
-                <img className="h-10 w-10" src={AddIcon} />
+              <img className="h-10 w-10" src={AddIcon} />
             </button>
-            </div>
-            </div>
-            <Hero/>
+          </div>
         </div>
-        </div>
-    )
+        <Hero />
+      </div>
+    </div>
+  );
 }
 
 export default header;
