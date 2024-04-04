38ca92b5f7582388176dc2c9924356b90ffc404c
Mohitsen11 <mohitsen0102@gmail.com>
2024-04-04 22:12:47 +0530

    feat-#38: Added email validation check + reduced the width + made changes in alignment for mobile screen

--- frontend/src/lib/types.ts
+++ frontend/src/lib/types.ts
@@ -1,7 +1,9 @@
 import { z } from "zod";
 
+const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+
 export const signInSchema = z.object({
-    email: z.string().email("Email is required"),
+    email: z.string().min(1, { message: "Email is required" }).regex(emailRegex, "Invalid email address"),
     password: z.string().min(1, { message: "Password is required" }),
 });
 
