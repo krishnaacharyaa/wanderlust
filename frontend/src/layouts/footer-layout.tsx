5ba08b999b240aa7d948ded61cb26fc1e68ed17e
Mohitsen11 <mohitsen0102@gmail.com>
2024-04-06 20:38:54 +0530

    feat-#38: Leveraged tailwind classes for colors + removed custom colors

--- frontend/src/layouts/footer-layout.tsx
+++ frontend/src/layouts/footer-layout.tsx
@@ -3,7 +3,7 @@ function footer() {
   const year = newDate.getFullYear();
 
   return (
-    <footer className="flex min-h-[6vh] flex-col items-center bg-footer-background py-2 text-center text-white sm:flex-row sm:justify-between sm:py-3">
+    <footer className="flex min-h-[6vh] flex-col items-center bg-zinc-900 py-2 text-center text-white sm:flex-row sm:justify-between sm:py-3">
       <section className="text-base sm:pl-4 md:pl-8 lg:pl-16">WanderLust</section>
       <section className="text-base sm:pr-4 md:pr-16 lg:pr-24">
         <span className="mr-2">&copy;</span>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -34,12 +34,6 @@ export default {
         'light-theme-foreground': '#f1c237',
         'dark-theme-background': '#092e40',
         'dark-theme-foreground': '#25a5e3',
-        'footer-background': '#181818',
-        'login-background': '#232323',
-        'input-background': '#F8F8F8',
-        'light-gray': '#686868',
-        'light-white': '#f9fafb',
-        'disabled-gray': '#3C3C3C',
       },
       fontFamily: {
         sans: ['Poppins', ...defaultTheme.fontFamily.sans],
