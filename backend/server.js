b05b4e11b1f91a21bd4443a12c15eec39ea65df2
krishnaacharyaa <krishnaacharya.official@gmail.com>
2024-04-09 13:48:33 +0530

    fix: Removed cors logic from the server.js as it causes deadlock in the preview deployment

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,16 @@
-import express from 'express';
-import postsRouter from './routes/posts.js';
-import authRouter from './routes/auth.js';
-import connectDB from './config/db.js';
-import cors from 'cors';
 import compression from 'compression';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import express from 'express';
+import connectDB from './config/db.js';
+import authRouter from './routes/auth.js';
+import postsRouter from './routes/posts.js';
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(
-  cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials:true,
-  })
-);
+app.use(cors());
 app.use(cookieParser());
 app.use(compression());
 
