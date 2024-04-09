b8144ac6fd0b585569479f3ebd9868c4ad22b6d2
krishnaacharyaa <krishnaacharya.official@gmail.com>
2024-04-09 13:20:02 +0530

    Revert "fix: Reverted back the cors change"
    This reverts commit f49c8249f7903df8dbcbd50bc02c47344848b779.

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,21 @@
+import express from 'express';
+import postsRouter from './routes/posts.js';
+import authRouter from './routes/auth.js';
+import connectDB from './config/db.js';
+import cors from 'cors';
 import compression from 'compression';
 import cookieParser from 'cookie-parser';
-import cors from 'cors';
-import express from 'express';
-import connectDB from './config/db.js';
-import authRouter from './routes/auth.js';
-import postsRouter from './routes/posts.js';
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials:true,
+  })
+);
 app.use(cookieParser());
 app.use(compression());
 
