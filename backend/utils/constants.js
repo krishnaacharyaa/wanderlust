bd1e11526271963c9095f03fc472d64e5d5781f3
Mohitsen11 <mohitsen0102@gmail.com>
2024-04-24 20:49:22 +0530

    feat: Removed hardcode response messages

--- backend/utils/constants.js
+++ backend/utils/constants.js
@@ -36,6 +36,8 @@ export const RESPONSE_MESSAGES = {
     USER_NOT_EXISTS: "User don't exist",
     AUTH_CODE_NOT_FOUND: 'Something went wrong',
     INVALID_PASSWORD: 'Invalid password.',
+    INVALID_TOKEN: 'Token is Invalid or expired!',
+    UNAUTHORIZED_USER: 'You are not authorized!',
   },
 };
 
