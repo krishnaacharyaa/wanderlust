93dad57a75b666200aad049a74ee427d75141a24
Aswinkumar2255 <mynameisaswin321@gmail.com>
2024-05-10 19:46:01 +0530

    feat-#4 : configured vitest and added unit test for signup component

---
+++ frontend/src/__tests__/Stringconstant.ts
@@ -0,0 +1,21 @@
+export const USERNAME_PLACEHOLDER = "Username"
+export const EMAILINPUT_PLACEHOLDER = "Email"
+export const PASSWORDINPUT_PLACEHOLDER = "Password"
+export const CONFIRMPASSWORD_PLACEHOLDER = "Confirm Password"
+export const SIGNUPBUTTON_TEXT = "Sign Up"
+
+export const INVALID_USERNAME_INPUT = "abcd"
+export const INVALID_EMAIL_INPUT = "abc@"
+export const INVALID_PWD_INPUT = "123"
+export const INVALID_CONFIRMPWD_INPUT = "1234"  
+
+export const INVALID_USERNAME_ERRORMESSAGE = "Username must be at least 5 characters long"
+export const INVALID_EMAIL_ERRORMESSAGE = "Invalid email address"
+export const INVALID_PWD_ERRORMESSAGE = "Password must be at least 8 characters long"
+export const INVALID_CONFIRMPWD_ERRORMESSAGE = "Passwords do not match"
+
+export const VALID_USERNAME_INPUT = "jhondeu"
+export const VALID_EMAIL_INPUT = "arya@gmail.com"
+export const VALID_PWD_INPUT = "123456789"
+export const VALID_CONFIRMPWD_INPUT = "123456789"  
+
---
+++ frontend/src/__tests__/unittest/signup/Client-util.tsx
@@ -0,0 +1,30 @@
+import { render } from "@testing-library/react"
+import { BrowserRouter } from "react-router-dom"
+import Signup from "@/pages/signup-page"
+import { CONFIRMPASSWORD_PLACEHOLDER, CONFIRMPWD_ERROR_ID, EMAILINPUT_PLACEHOLDER, EMAIL_ERROR_ID, PASSWORDINPUT_PLACEHOLDER, PWD_ERROR_ID, SIGNUPBUTTON_TEXT, USERNAME_ERROR_ID, USERNAME_PLACEHOLDER } from "@/__tests__/Stringconstant"
+
+
+export const formSetup = ()=>{
+
+    
+    const form = render(
+       <BrowserRouter>
+       <Signup />
+       </BrowserRouter>
+    )
+
+    const usernameInput = form.getByPlaceholderText(USERNAME_PLACEHOLDER)
+    const emailInput = form.getByPlaceholderText(EMAILINPUT_PLACEHOLDER)
+    const passwordInput = form.getByPlaceholderText(PASSWORDINPUT_PLACEHOLDER)
+    const confirmpasswordInput = form.getByPlaceholderText(CONFIRMPASSWORD_PLACEHOLDER)
+    const signupbuttonText = form.getByText(SIGNUPBUTTON_TEXT)
+  
+    if(!(usernameInput instanceof HTMLInputElement) || !(emailInput instanceof HTMLInputElement) || !(passwordInput instanceof HTMLInputElement) || !(confirmpasswordInput instanceof HTMLInputElement) || !(signupbuttonText instanceof HTMLButtonElement) ){
+
+    throw new Error("Issue during test setup, some input elemnts are not rendered")
+
+    }
+
+    return {form,usernameInput,emailInput,passwordInput,confirmpasswordInput,signupbuttonText}
+
+}
