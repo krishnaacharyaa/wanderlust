ad48b1fc44b6c18a036b44d07d492cbd3dcba8e4
Aswinkumar2255 <mynameisaswin321@gmail.com>
2024-05-13 11:20:04 +0530

    chore-#4 : added coverage options and verbose for detailed test report

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,24 @@
 import { defineConfig,UserConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
+import { configDefaults } from 'vitest/config';
 
 export default defineConfig({
   plugins: [react()] as UserConfig["plugins"],
   test: {
     environment: 'jsdom',
     globals: true,
+    coverage:{
+      provider : 'v8',
+    },
+    reporters:['verbose'],
+    exclude:[
+...configDefaults.exclude,
+'./src/__tests__/integration/home.test.tsx',
+'./src/__tests__/App.test.tsx'
+    ],
+   
+      
     setupFiles: './test-setup.ts',
   },
 
