3511b5b7eea9367a6dbbdfebb2ca20a15073ca43
Sukomal07 <hypersukomal@gmail.com>
2024-05-14 10:13:04 +0530

    fix : fix signin , signup , home page code , added axios-instance and require-auth and loader

--- frontend/src/layouts/header-layout.tsx
+++ frontend/src/layouts/header-layout.tsx
@@ -2,110 +2,151 @@ import ThemeToggle from '@/components/theme-toggle-button';
 import AddIcon from '@/assets/svg/add-icon-white.svg';
 import LogOutIcon from '@/assets/svg/logout-icon.svg';
 import LogInIcon from '@/assets/svg/login-icon.svg';
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import Hero from '@/components/hero';
-import { useEffect, useState } from 'react';
-import axios from 'axios';
+import { AxiosError, isAxiosError } from 'axios';
 import { toast } from 'react-toastify';
-import userState from '@/utils/user-state';
+import axiosInstance from '@/helpers/axios-instance';
+import { useEffect, useState } from 'react';
+import Loader from '@/components/skeletons/loader';
 function header() {
   const navigate = useNavigate();
+  const location = useLocation()
+  const [data, setData] = useState({
+    _id: localStorage.getItem("userId") || '',
+    token: '',
+    loading: true
+  })
 
-  const [accessToken, setAccessToken] = useState<string | null>(userState.getUser());
-
-  useEffect(() => {
-    const storedAccessToken = userState.getUser();
-
-    if (storedAccessToken) {
-      setAccessToken(storedAccessToken);
-    }
-  }, [accessToken, userState]);
 
   const handleLogout = async () => {
     try {
-      const response = await axios.post(import.meta.env.VITE_API_PATH + '/api/auth/signout');
-
-      if (response.status === 200) {
-        toast.success(response.data.message);
-        userState.setUser(null);
-        setAccessToken(null);
-        navigate('/');
-      } else {
-        toast.error('Error: ' + response.data.message);
+      const response = axiosInstance.post('/api/auth/signout')
+      toast.promise(response, {
+        pending: 'Wait ...',
+        success: {
+          render({ data }) {
+            localStorage.removeItem("userId")
+            localStorage.removeItem("role")
+            navigate('/');
+            return data?.data?.message
+          },
+        },
+        error: {
+          render({ data }) {
+            if (data instanceof AxiosError) {
+              if (data?.response?.data?.message) {
+                return data?.response?.data?.message;
+              }
+            }
+            return "Signout failed"
+          },
+        },
       }
-    } catch (err: any) {
-      console.log('Error :', err.message);
-      toast.error('Something went wrong. Please try again later.');
+      )
+
+      return (await response).data
+    } catch (error) {
+      if (isAxiosError(error)) {
+        console.error(error.response?.data?.message || 'An error occurred');
+      } else {
+        console.error(error);
+      }
     }
   };
+
+  useEffect(() => {
+    async function fetchToken() {
+      try {
+        const res = await axiosInstance.get(`/api/auth/check/${data._id}`)
+        setData({
+          ...data,
+          token: res.data?.data,
+          loading: false
+        })
+      } catch (error) {
+        setData({
+          ...data,
+          token: '',
+          loading: false
+        })
+      }
+    }
+    fetchToken()
+  }, [location])
+
   return (
     <div className="relative -mt-2 h-[460px] bg-[url('./assets/wanderlustbg.webp')] bg-cover bg-fixed bg-center">
       <div className="absolute inset-0 bg-black opacity-50"></div>
-      <div className="absolute inset-0 flex flex-col px-4 py-8 text-slate-50 sm:px-16">
+      <div className="absolute inset-0 flex flex-col px-4 py-8 text-slate-50 md:px-16">
         <div className="flex w-full justify-between">
           <div className="flex cursor-text items-center justify-between text-2xl font-semibold">
             WanderLust
           </div>
-          <div className="flex justify-between">
-            <div className="flex items-center justify-end px-2 py-2 sm:px-20">
+          <div className="flex justify-between items-center">
+            <div className="flex items-center justify-end md:pr-20">
               <ThemeToggle />
             </div>
-            {accessToken ? (
-              <div className="flex gap-2 ">
-                <button
-                  className="active:scale-click hidden rounded border border-slate-50 px-4 py-2 hover:bg-slate-500/25 sm:inline-block"
-                  onClick={() => {
-                    navigate('/add-blog');
-                  }}
-                >
-                  Create post
-                </button>
-                <button
-                  className="active:scale-click hidden rounded border border-slate-50 px-4 py-2 hover:bg-slate-500/25 sm:inline-block"
-                  onClick={() => {
-                    handleLogout();
-                  }}
-                >
-                  Logout
-                </button>
-                <button
-                  className="px-2 py-2 hover:bg-slate-500/25 sm:hidden"
-                  onClick={() => {
-                    navigate('/add-blog');
-                  }}
-                >
-                  <img className="h-10 w-10" src={AddIcon} />
-                </button>
-                <button
-                  className="py-2 hover:bg-slate-500/25 sm:hidden sm:px-2"
-                  onClick={() => {
-                    handleLogout();
-                  }}
-                >
-                  <img className="h-9 w-9" src={LogOutIcon} />
-                </button>
-              </div>
-            ) : (
-              <div className="flex">
-                {' '}
-                <button
-                  className="active:scale-click hidden rounded border border-slate-50 px-4 py-2 hover:bg-slate-500/25 sm:inline-block"
-                  onClick={() => {
-                    navigate('/signin');
-                  }}
-                >
-                  Login
-                </button>
-                <button
-                  className="py-2 hover:bg-slate-500/25 sm:hidden sm:px-2"
-                  onClick={() => {
-                    navigate('/signin');
-                  }}
-                >
-                  <img className="h-9 w-9" src={LogInIcon} />
-                </button>
-              </div>
-            )}
+            <div>
+              {data.loading ? (
+                <Loader />
+              ) : data.token ? (
+                <div className="flex gap-2 ">
+                  <button
+                    className="active:scale-click rounded border border-slate-50 px-4 py-2 hover:bg-slate-500/25 md:inline-block"
+                    onClick={() => {
+                      navigate('/add-blog');
+                    }}
+                  >
+                    Create post
+                  </button>
+                  <button
+                    className="active:scale-click rounded border border-slate-50 px-4 py-2 hover:bg-slate-500/25 md:inline-block"
+                    onClick={() => {
+                      handleLogout();
+                    }}
+                  >
+                    Logout
+                  </button>
+                  <button
+                    className="px-2 py-2 hover:bg-slate-500/25 md:hidden"
+                    onClick={() => {
+                      navigate('/add-blog');
+                    }}
+                  >
+                    <img className="h-10 w-10" src={AddIcon} alt="Add Icon" />
+                  </button>
+                  <button
+                    className="py-2 hover:bg-slate-500/25 md:hidden md:px-2"
+                    onClick={() => {
+                      handleLogout();
+                    }}
+                  >
+                    <img className="h-9 w-9" src={LogOutIcon} alt="Logout Icon" />
+                  </button>
+                </div>
+              ) : (
+                <div className="flex">
+                  {' '}
+                  <button
+                    className="active:scale-click rounded border border-slate-50 px-4 py-2 hover:bg-slate-500/25 md:inline-block"
+                    onClick={() => {
+                      navigate('/signin');
+                    }}
+                  >
+                    Login
+                  </button>
+                  <button
+                    className="py-2 hover:bg-slate-500/25 md:hidden md:px-2"
+                    onClick={() => {
+                      navigate('/signin');
+                    }}
+                  >
+                    <img className="h-9 w-9" src={LogInIcon} alt="Login Icon" />
+                  </button>
+                </div>
+              )}
+            </div>
           </div>
         </div>
         <Hero />
