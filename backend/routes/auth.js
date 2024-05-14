f3550fa5cc4cfbc4e7c630da9366ccc9967509f2
Sukomal07 <hypersukomal@gmail.com>
2024-05-14 09:52:06 +0530

    added : aded isloggedin for checking user

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -10,6 +10,7 @@ import {
   signUpWithGithub,
   signInWithGithub,
   signOutUser,
+  isLoggedIn,
 } from '../controllers/auth-controller.js';
 
 const router = Router();
@@ -31,5 +32,7 @@ router.post('/email-password/signin', signInWithEmailOrUsername);
 //SIGN OUT
 router.post('/signout', authMiddleware, signOutUser);
 
+//CHECK USER STATUS
+router.get('/check/:_id', isLoggedIn)
 
 export default router;
