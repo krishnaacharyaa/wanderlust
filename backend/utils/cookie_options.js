a85a16e5a0c39f1d6ddf9120c9c0afe57a2287f3
Sukomal07 <hypersukomal@gmail.com>
2024-05-14 08:52:59 +0530

    fix: fix signup , signin , signout and auth-middleware, added user methods

--- backend/utils/cookie_options.js
+++ backend/utils/cookie_options.js
@@ -1,14 +1,8 @@
-import { ACCESS_COOKIE_MAXAGE, REFRESH_COOKIE_MAXAGE } from '../config/utils.js';
+import { ACCESS_COOKIE_MAXAGE, NODE_ENV } from '../config/utils.js';
 
-export const accessCookieOptions = {
+export const cookieOptions = {
   httpOnly: true,
-  sameSite: 'none',
-  secure: true,
+  sameSite: NODE_ENV === "Development" ? "lax" : "strict",
+  secure: NODE_ENV === "Development" ? false : true,
   maxAge: ACCESS_COOKIE_MAXAGE,
-};
-export const refreshCookieOptions = {
-  httpOnly: true,
-  sameSite: 'none',
-  secure: true,
-  maxAge: REFRESH_COOKIE_MAXAGE,
 };
