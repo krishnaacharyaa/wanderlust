8210822681e341bb2b06f001f77178011d07c3cf
Sukomal07 <hypersukomal@gmail.com>
2024-05-14 08:52:59 +0530

    fix: fix signup , signin , signout and auth-middleware, added user methods

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,8 +1,8 @@
 import { Router } from 'express';
-const router = Router();
+import { authMiddleware } from '../middlewares/auth-middleware.js';
 import {
   signUpWithEmail,
-  signInWithEmail,
+  signInWithEmailOrUsername,
   openGoogleAuthWindow,
   signUpWithGoogle,
   signInWithGoogle,
@@ -11,6 +11,8 @@ import {
   signInWithGithub,
   signOutUser,
 } from '../controllers/auth-controller.js';
+
+const router = Router();
 
 //GOOGLE STRATEGY
 router.get('/google', openGoogleAuthWindow);
@@ -24,9 +26,10 @@ router.get('/github/signin/callback', signInWithGithub);
 
 //REGULAR EMAIL PASSWORD STRATEGY
 router.post('/email-password/signup', signUpWithEmail);
-router.post('/email-password/signin', signInWithEmail);
+router.post('/email-password/signin', signInWithEmailOrUsername);
 
 //SIGN OUT
-router.post('/signout', signOutUser);
+router.post('/signout', authMiddleware, signOutUser);
+
 
 export default router;
