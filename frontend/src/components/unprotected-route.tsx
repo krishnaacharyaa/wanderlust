a26ab3f413bdede98106ff187a251b0069991c01
Sukomal07 <hypersukomal@gmail.com>
2024-05-15 14:19:14 +0530

    fix: fix signout problem, added custom hook and types

--- frontend/src/components/unprotected-route.tsx
+++ frontend/src/components/unprotected-route.tsx
@@ -1,40 +1,10 @@
-import axiosInstance from '@/helpers/axios-instance';
-import { useEffect, useState } from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
-import Loader from './skeletons/loader';
+import useAuthData from '@/hooks/useAuthData';
 
 function UnprotectedRoute() {
-    const [data, setData] = useState({
-        _id: localStorage.getItem("userId") || '',
-        token: '',
-        loading: true
-    })
+    const { token } = useAuthData();
 
-    useEffect(() => {
-        async function fetchToken() {
-            try {
-                const res = await axiosInstance.get(`/api/auth/check/${data._id}`)
-                setData({
-                    ...data,
-                    token: res.data?.data,
-                    loading: false
-                })
-            } catch (error) {
-                setData({
-                    ...data,
-                    token: '',
-                    loading: false
-                })
-            }
-        }
-        fetchToken()
-    }, [])
-
-    if (data.loading) {
-        return <><Loader /></>; // Render a loading indicator
-    }
-
-    return data.token ? <Navigate to="/" /> : <Outlet />;
+    return token ? <Navigate to="/" /> : <Outlet />;
 }
 
 export default UnprotectedRoute
