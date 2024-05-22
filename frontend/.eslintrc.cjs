87523dadd58c22bf3c3b5f7bf3d783e676e9479c
Ameer jafar <ameerjafar123@gmail.com>
2024-05-22 19:00:04 +0530

    feat-#177: Added the eslint in the husky

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -6,12 +6,22 @@ module.exports = {
     'plugin:@typescript-eslint/recommended-type-checked',
     'plugin:react-hooks/recommended',
     'plugin:@typescript-eslint/stylistic-type-checked',
+    'airbnb-base',
     'prettier',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'node_modules/'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: [
+    'react-refresh',
+    '@typescript-eslint', 
+    'import', 
+  ],
+  parserOptions: {
+    project: './tsconfig.json', // Point to your tsconfig.json file
+    tsconfigRootDir: __dirname,
+  },
   rules: {
     'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+
   },
 };
