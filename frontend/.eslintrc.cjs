990f55025647c66a278743738791eeb04fe52a7b
Ameer jafar <ameerjafar123@gmail.com>
2024-05-23 18:34:12 +0530

    chore: revert to prettier one

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -10,26 +10,9 @@ module.exports = {
   ignorePatterns: ['dist', '.eslintrc.cjs', 'node_modules/'],
   parser: '@typescript-eslint/parser',
   plugins: [
-    'react-refresh', 
-    '@stylistic/js'
+    'react-refresh'
   ],
   rules: {
-    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
-    '@stylistic/js/indent': [
-      'error',
-      2
-    ],
-  '@stylistic/js/linebreak-style': [
-      'error',
-      'unix'
-    ],
-  '@stylistic/js/quotes': [
-      'error',
-      'single'
-    ],
-  '@stylistic/js/semi': [
-      'error',
-      'never'
-    ],
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }]
   },
 };
