fcd701e0ea39afb44f884b8f6b79cf2bc7b800b8
Ameer jafar <ameerjafar123@gmail.com>
2024-05-23 19:42:48 +0530

    chore: added the prettier recommended

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -6,6 +6,7 @@ module.exports = {
     'plugin:@typescript-eslint/recommended-type-checked',
     'plugin:react-hooks/recommended',
     'plugin:@typescript-eslint/stylistic-type-checked',
+    'plugin:prettier/recommended'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs', 'node_modules/'],
   parser: '@typescript-eslint/parser',
