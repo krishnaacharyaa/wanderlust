820c1dd15410dc8a3c9e17ff442f7208c5e28438
Rushil <rushil13579@gmail.com>
2024-05-25 13:22:30 +0530

    fix-#358: set json response limit

--- backend/app.js
+++ backend/app.js
@@ -15,7 +15,7 @@ app.use(cors({
     origin: FRONTEND_URL,
     credentials: true
 }));
-app.use(express.json());
+app.use(express.json( { limit: '50kb' }));
 app.use(express.urlencoded());
 app.use(cookieParser());
 app.use(compression());
