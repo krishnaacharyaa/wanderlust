001e047c1c956b1160cb6f1f6a5c928e8c3299ad
Chelsea <chelseasingla123@gmail.com>
2024-05-28 15:14:31 +0530

    Update post-card-skeleton.tsx

--- frontend/src/components/skeletons/post-card-skeleton.tsx
+++ frontend/src/components/skeletons/post-card-skeleton.tsx
@@ -6,15 +6,15 @@ export const PostCardSkeleton = () => {
       <div className="mb-4 mr-8 mt-4 rounded-lg bg-light shadow-md dark:bg-dark-card">
         <Skeleton className="h-48 w-full rounded-lg bg-slate-200 dark:bg-slate-700" />
         <div className="p-4">
-          <Skeleton className="mb-2 h-3 w-full bg-slate-200 dark:bg-slate-700 sm:w-2/3" />
-          <Skeleton className="mb-2 h-6 w-full bg-slate-200 dark:bg-slate-700 sm:w-4/5" />
-          <Skeleton className="h-16 w-full bg-slate-200 dark:bg-slate-700 sm:w-11/12" />
-          <div className="mt-2 flex flex-wrap gap-2">
+          <Skeleton className="mb-2 h-3 w-full pr-2 bg-slate-200 dark:bg-slate-700" />
+          <Skeleton className="mb-2 h-6 w-full pr-2 bg-slate-200 dark:bg-slate-700" />
+          <Skeleton className="mb-2 sm:mb-4 h-6 w-full pr-2 bg-slate-200 dark:bg-slate-700" />
+          <div className="mt-1 sm:mt-2 flex flex-wrap gap-1 sm:gap-1.5">
             <Skeleton
-              className={`h-6 w-full rounded-full sm:w-16 ${'sm:mr-8 sm:mt-4'} bg-slate-200 dark:bg-slate-700 sm:mb-4`}
+              className={`h-6 w-16 rounded-full bg-slate-200 dark:bg-slate-700`}
             />
             <Skeleton
-              className={`h-6 w-full rounded-full sm:w-16 ${'sm:mr-8 sm:mt-4'} bg-slate-200 dark:bg-slate-700 sm:mb-4`}
+              className={`h-6 w-16 rounded-full bg-slate-200 dark:bg-slate-700`}
             />
           </div>
         </div>
