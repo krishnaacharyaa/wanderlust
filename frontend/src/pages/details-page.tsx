9e344f0097c8365854b39b8ffdd1c01e540186d8
vamsidhar-914 <vamsimay12345@gmail.com>
2024-06-04 16:40:15 +0530

    fix-#392: added related blogs section in detailed blog page

--- frontend/src/pages/details-page.tsx
+++ frontend/src/pages/details-page.tsx
@@ -1,9 +1,14 @@
-import { useLocation, useNavigate, useParams } from 'react-router-dom';
+import { Link, useLocation, useNavigate, useParams } from 'react-router-dom';
 import navigateBackWhiteIcon from '@/assets/svg/navigate-back-white.svg';
 import formatPostTime from '@/utils/format-post-time';
 import CategoryPill from '@/components/category-pill';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+import Post from '@/types/post-type';
+import axiosInstance from '@/helpers/axios-instance';
+import { ArrowBigRight } from 'lucide-react';
+import { PostCardSkeleton } from '@/components/skeletons/post-card-skeleton';
+import PostCard from '@/components/post-card';
 
 export default function DetailsPage() {
   const { state } = useLocation();
@@ -12,6 +17,8 @@ export default function DetailsPage() {
   const [loading, setIsLoading] = useState(initialVal);
   const { postId } = useParams();
   const navigate = useNavigate();
+  const [categoryPosts, setcategoryPosts] = useState<Post[]>([]);
+  const [postLoading, setpostLoading] = useState(false);
 
   useEffect(() => {
     const getPostById = async () => {
@@ -29,6 +36,24 @@ export default function DetailsPage() {
       getPostById();
     }
   }, [post]);
+
+  const category = post.categories[0];
+  useEffect(() => {
+    const fetchCategoryPost = async () => {
+      try {
+        setpostLoading(true);
+        const res = await axiosInstance.get(`/api/posts/categories/${category}`);
+        const filterdPosts = res.data.filter((item: Post) => item._id !== post._id);
+        const posts = filterdPosts.slice(0, 4);
+        setcategoryPosts(posts);
+        setpostLoading(false);
+      } catch (err) {
+        console.log(err);
+        setpostLoading(false);
+      }
+    };
+    fetchCategoryPost();
+  }, [category]);
 
   if (!loading)
     return (
@@ -64,6 +89,24 @@ export default function DetailsPage() {
             <p className="leading-7 text-light-secondary dark:text-dark-secondary sm:text-lg">
               {post.description}
             </p>
+          </div>
+        </div>
+        <div className="container mx-auto flex flex-col px-4 py-6 text-white">
+          <div className="flex justify-between text-2xl font-semibold">
+            <div>Related Blogs</div>
+            <div className="mr-8 flex cursor-pointer items-center gap-1 text-gray-400 hover:underline">
+              <Link to="/">
+                <div className="text-sm">see more blogs</div>
+              </Link>
+              <ArrowBigRight className="h-6 w-6" />
+            </div>
+          </div>
+          <div className="flex flex-wrap">
+            {categoryPosts.length === 0 && postLoading
+              ? Array(4)
+                  .fill(0)
+                  .map((_, index) => <PostCardSkeleton key={index} />)
+              : categoryPosts.map((post) => <PostCard key={post._id} post={post} />)}
           </div>
         </div>
       </div>
